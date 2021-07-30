e99b3f1907ce63868901e75cfec815c9a1fc042a
Victor <victorrivera714@yahoo.com>
2021-07-29 20:39:02 -0700

    edit index.js

--- index.js
+++ index.js
@@ -1,16 +1,91 @@
+    document.addEventListener('DOMContentLoaded', () => { // callback function, asychronous, arrow function
+        document.getElementById('universities').addEventListener('click', university) // when you click 
+        university();
+        changeBackground();
+        
+
+    })
+
+    function university (){
+        let info = document.getElementById('info') 
+        let universities  = document.getElementById("university-list") 
+        info.innerHTML = "" 
+        universities.innerHTML = "" 
+        fetch ("http://universities.hipolabs.com/search?country=United+Kingdom")
+        .then(res => res.json())
+        .then(data => {  
+            data.map(uni => { 
+                universities.innerHTML += ` 
+                <li>
+                <a href="#" data-name="${uni.name}"
+                data-country="${uni.country}"
+                > ${uni.name} </a>  
+                
+                <button data-likes = "0"  class = "buttons"> like!</button>
+                    </li>
+                `
+
+            }) 
+            
+            
+            
+            
+          schoolLinks() 
+        })
+    };
+
+     function schoolLinks(){ 
+        const buttonLiker = document.querySelectorAll(`button.buttons`) 
+        buttonLiker.forEach(college => { 
+            college.addEventListener('click', likeSchool) 
+        })
 
 
-document.addEventListener('DOMContentLoaded', ()=> {
-    university();
+        const campuses = document.querySelectorAll(`li a`) 
+        campuses.forEach(uni => { 
+            uni.addEventListener('click', showUni) 
+         })  
+    }
 
-})
 
-function university(){
-    fetch ("http://universities.hipolabs.com/search?country=United+Kingdom")
-    .then(res => res.json())
-    .then(data => renderSchools (data.results));
-};
+        function likeSchool(e){
+            
+            const li = e.target.parentElement 
+            const button = e.target  
+            const likes = li.querySelector(`p`)  
+            button.dataset.likes ++ 
+            
 
-const schools = (info) => {
-    console.log(info);
-};
+           
+            if (likes === null){ 
+                li.insertAdjacentHTML(`beforeend`, `<p id = "likes" > Likes: ${button.dataset.likes}</p>`) 
+                
+            }
+            else {
+                likes.innerText = `Likes: ${button.dataset.likes}` 
+            }
+        }
+
+
+
+     function showUni(event){ 
+       
+        let a = event.target 
+        let universities = document.getElementById('university-list') 
+        universities.innerHTML = `
+                                    <h2>School Name:</h2>
+                                    <p>${a.dataset.name}</p>
+                                    <br>
+                                    <h3>Country Name:</h3>
+                                    <p>${a.dataset.country}</p>`
+          
+    }
+
+
+
+        
+        
+        
+        
+        
+        
